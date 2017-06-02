c7f5995a3e5ab16a5947cfb7fc84db7ba597aa82
tomek <tomek@debian>
2017-06-02 15:19:06 +0200

    spriteBackGround

--- main.cpp
+++ main.cpp
@@ -9,6 +9,16 @@ int main()
   VideoMode vm(1920,1080);
   RenderWindow window(vm,"MyTimer", Style::Fullscreen);
   
+  Texture textureBackGround;
+  
+  textureBackGround.loadFromFile("graphics/background.png");
+  
+  Sprite spriteBackGround;
+  
+  spriteBackGround.setTexture(textureBackGround);
+  
+  spriteBackGround.setPosition(0,0);
+  
   while(window.isOpen())
   {
     if(Keyboard::isKeyPressed(Keyboard::Escape))
@@ -17,6 +27,8 @@ int main()
     }
     
     window.clear();
+    
+    window.draw(spriteBackGround);
     
     window.display();
   }
